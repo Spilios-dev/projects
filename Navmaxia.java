175959e0f94e77aa7fb9450540b5e374a9634ce1
Spilios-dev <61265986+Spilios-dev@users.noreply.github.com>
2020-02-20 08:45:44 +0200

    Add files via upload

---
+++ Navmaxia.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package navmaxia;
+
+/**
+ *
+ * @author user
+ */
+public class Navmaxia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Frame_Navmaxias fn = new Frame_Navmaxias();
+        fn.setVisible(true);
+    }
+    
+}
